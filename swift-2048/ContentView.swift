741e867391b44ce9286e9f5ddc819967a35efb4f
LioMasterRIE <contact@cmli.fr>
2022-03-30 15:08:04 +0200

    ok

---
+++ swift-2048/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift-2048
+//
+//  Created by Lionel FERRAFIAT on 30/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-2048/Swift_2048App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_2048App.swift
+//  Swift-2048
+//
+//  Created by Lionel FERRAFIAT on 30/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Swift_2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
