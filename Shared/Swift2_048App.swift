64046a40f1663cb4e4b0a6a7ae292cb0b656fdb9
LioMasterRIE <contact@cmli.fr>
2022-03-30 12:25:47 +0200

    go

---
+++ Shared/Swift2_048App.swift
@@ -0,0 +1,18 @@
+//
+//  Swift2_048App.swift
+//  Shared
+//
+//  Created by Lionel FERRAFIAT on 30/03/2022.
+//  Copyright © 2022 Austin Zheng. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Swift2_048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
